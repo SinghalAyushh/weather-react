fc30669e2cddd5bab44c88f23f2c89e3563c7344
djangobaby2015 <68475187+djangobaby2015@users.noreply.github.com>
2020-08-01 01:46:12 -0700

    Updates

---
+++ precache-manifest.87ffcf24d885dce3fb8c6b88193f5fe8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accf136f0a44c6603b7a70653d4cf246",
+    "url": "/weather-react.git/index.html"
+  },
+  {
+    "revision": "a2325334724ae19e794b",
+    "url": "/weather-react.git/static/css/2.16cac14c.chunk.css"
+  },
+  {
+    "revision": "70dffb7b5d27f48461a1",
+    "url": "/weather-react.git/static/css/main.c266520e.chunk.css"
+  },
+  {
+    "revision": "a2325334724ae19e794b",
+    "url": "/weather-react.git/static/js/2.b131dd08.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/weather-react.git/static/js/2.b131dd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70dffb7b5d27f48461a1",
+    "url": "/weather-react.git/static/js/main.8b200df9.chunk.js"
+  },
+  {
+    "revision": "9c78b253239260699c75",
+    "url": "/weather-react.git/static/js/runtime-main.d083b491.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/weather-react.git/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/weather-react.git/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/weather-react.git/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/weather-react.git/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/weather-react.git/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/weather-react.git/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/weather-react.git/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/weather-react.git/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/weather-react.git/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/weather-react.git/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/weather-react.git/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/weather-react.git/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/weather-react.git/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/weather-react.git/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/weather-react.git/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/weather-react.git/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
