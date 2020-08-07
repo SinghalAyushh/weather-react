1d903a423787f60217443070372e8949e56c6ad4
djangobaby2015 <68475187+djangobaby2015@users.noreply.github.com>
2020-08-07 10:10:17 -0700

    ad

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
 
   return (
    
-    <div className="App" style ={{  backgroundImage:`url(${src})`}}>
+    <div className="App" style ={{  backgroundImage:`url(${src})` }}>
      
       <div className="Navbar" >
     
